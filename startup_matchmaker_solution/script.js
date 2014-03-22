e07bc75a24de6eae0c5b006f8391b6dd4c32a6c2
evabrandenburg <evabrandenburg89@gmail.com>
2014-03-22 16:02:41 -0400

    bootstrap

---
+++ startup_matchmaker_solution/script.js
@@ -0,0 +1,13 @@
+(function(){
+
+	// find element to add click event to
+	var mobileButton = $( .'mobile-nav-button' );
+
+	//save the mobile version of the nav to a variable as well 
+	var mobileNav = $( '.mobile-nav-tree' );
+
+	//wire up a click event
+
+	//when ckick occurs, do something
+	
+})();
